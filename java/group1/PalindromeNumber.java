64ffaa5ad5702267efe47b6fc7921e387e646a88
bpupadhyaya <bpupadhyaya@gmail.com>
2022-12-05 22:30:47 -0800

    Detect if a given integer is a palindrome or not.

---
+++ java/group1/PalindromeNumber.java
@@ -0,0 +1,24 @@
+// Detect if a given integer is a palindrome or not.
+// Sample input: 12321 : palindrome, input: 12345 : not a palindrome, input -12321 : not a palindrome
+
+class PalindromeNum {
+    public static void main(String[] args){
+        int inputNum = 12321;
+        System.out.println("Result: " + isPalinNum(inputNum));
+    }
+
+    private static boolean isPalinNum(int inputNum) {
+        String input = inputNum+"";
+        int len = input.length();
+        boolean palin = true;
+        if (len % 2 == 0)
+            return false;
+        for (int i = 0; i < len / 2; i++) {
+            if (input.charAt(i) != input.charAt(len - i -1 ))
+                palin = false;
+            else
+                continue;
+        }
+        return palin;
+    }
+}

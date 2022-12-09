ce325eabd28159029adde1eac9dd46954ee17083
bpupadhyaya <bpupadhyaya@gmail.com>
2022-12-09 13:42:40 -0800

    Convert a given interger to a roman numeral.

---
+++ java/group2/IntegerToRoman1.java
@@ -0,0 +1,18 @@
+// Convert a given interger to a roman numeral.
+// Time complexity: O(1), space complexity: O(1)
+
+class IntegerToRoman1 {
+    public static void main(String[] args) {
+        int myInput = 555;
+        System.out.println("Equivalent roman: " + convertIntToRoman(myInput));
+    }
+
+    private static String convertIntToRoman(int input) {
+        final String[] thousands = {"", "M", "MM", "MMM"};
+        final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+        return thousands[input / 1000] + hundreds[input % 1000 / 100] + tens[input % 100 / 10] + ones[input % 10];
+    }
+}

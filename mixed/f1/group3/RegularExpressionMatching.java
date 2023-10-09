79bb20aa2c698d8b621b8fb97e87b4860678d6f6
bpupadhyaya <bpupadhyaya@gmail.com>
2023-10-08 18:26:35 -0700

    Given an input string and a pattern, implement regular expression matching with support for '.' and '*', where '.' matches any single character and '*' matches zero or more of the preceding element. The matching should cover the entire input string.

---
+++ mixed/f1/group3/RegularExpressionMatching.java
@@ -0,0 +1,42 @@
+// Given an input string and a pattern, implement regular expression matching with support for '.'
+// and '*', where '.' matches any single character and '*' matches zero or more of the preceding element.
+// The matching should cover the entire input string.
+// Sample 1:
+// Input: s = "xx", p = "x"
+// Output: false, explanation: "x" does not match the entire string "xx"
+// Sample 2:
+// Input s  = "xx", p = "x*"
+// Output: true
+// Sample 3:
+// Input: s = "xy", p = ".*"
+// Ouput: true, explanation: ".*" means zero or more of any character.
+
+class RegularExpressionMatching {
+    public static void main(String[] args) {
+        String s = "xx";
+        String p = "x";
+        System.out.println("Is match? " + isMatch(s, p));
+    }
+
+    static boolean isMatch(String s, String p) {
+        if (p == null || p.length() == 0)
+            return (s == null || s.length() == 0);
+
+        boolean dp[][] = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int j=2; j <= p.length(); j++) {
+            dp[0][j] = p.charAt(j-1) == '*' && dp[0][j-2];
+        }
+
+        for (int j=1; j <= p.length(); j++) {
+            for (int i=1; i <= s.length(); i++) {
+                if (p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.')
+                    dp[i][j] = dp[i-1][j-1];
+                else if (p.charAt(j-1) == '*')
+                    dp[i][j] = dp[i][j-2] || ((s.charAt(i-1) == p.charAt(j-2) || p.charAt(j-2) == '.') && dp[i-1][j]);
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+}

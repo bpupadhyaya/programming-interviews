cf48d446baa6469613a5679b92fc12dfe8e08a61
bpupadhyaya <bpupadhyaya@gmail.com>
2023-10-26 14:11:55 -0700

    Binary tree vertical order traversal.

---
+++ mixed/f1/group2/BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,74 @@
+// Given the root of a binary tree, return the vertical order traversal of its nodes' values. (i.e., from top to bottom,
+// column by column).
+// If two nodes are in the same row and column, the order should be from left to right.
+// Sample 1:
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[9],[3,15],[20],[7]]
+// Note: visualize to understand
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.LinkedList;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {this.val = val;}
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinaryTreeVerticalOrderTraversal {
+    public static void main(String...args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20));
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> vertOrder = verticalOrder(root);
+        for (List<Integer> elem: vertOrder)
+            System.out.print(elem);
+        System.out.println();
+    }
+
+    static List<List<Integer>> verticalOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Map<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        Map<TreeNode, Integer> weight = new HashMap<TreeNode, Integer>();
+        queue.offer(root);
+        weight.put(root,0);
+        int min = 0;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            int w = weight.get(node);
+            if (!map.containsKey(w)) {
+                map.put(w, new ArrayList<>());
+            }
+            map.get(w).add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+                weight.put(node.left, w-1);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+                weight.put(node.right, w+1);
+            }
+            min = Math.min(min, w);
+        }
+        while (map.containsKey(min)) {
+            res.add(map.get(min++));
+        }
+        return res;
+    }
+}

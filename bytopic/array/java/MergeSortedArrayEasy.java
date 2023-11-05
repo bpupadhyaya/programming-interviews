60d2e65625f221113ec301c7fe85027ebe03a01f
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-05 14:55:20 -0800

    Merge two sorted arrays, append and sort method.

---
+++ bytopic/array/java/MergeSortedArrayEasy.java
@@ -0,0 +1,41 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing
+// the number of elements in nums1 and nums2 respectively.
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate
+// this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Soln: two pointer
+// Time complexity ~ O((m+n)log(m+n)), due to sort() function
+// Space complexity ~ O(1)
+
+import java.util.Arrays;
+class MergeSortedArrayEasy {
+    public static void main(String...args) {
+        int[] nums1 = {4,5,8,0,0,0,0};
+        int m = 3;
+        int[] nums2 = {6,7,8,9};
+        int n = 4;
+        merge(nums1, m, nums2, n);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.print(nums1[i] + ",");
+        }
+        System.out.println();
+
+    }
+
+    static void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+
+// Soln idea / algo:
+// 1. Traverse through nums2 and append its elements to the end of nums1 starting from index m.
+// 2. Sort the entire nums1 array using library function, sort() in this case.

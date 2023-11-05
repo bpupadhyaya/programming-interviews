89c124c856c2fdde04dea6b74c2ca3c29b8735c7
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-04 23:31:19 -0700

    Initial folders structure for by-topic programming problems collection.

---
+++ mixed/f1/misc/CharTest.java
@@ -0,0 +1,12 @@
+class CharTest {
+    public static void main(String...args) {
+        charTest();
+    }
+
+    private static void charTest() {
+        int a  = '1' - '0';
+        int b = 0;
+        System.out.println(a+b);
+    }
+
+}

b7a4159d2d15cedb68d6783dcffbb9b71e1eed64
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-05 16:24:25 -0800

    Correction: append sort method.

--- bytopic/array/java/MergeSortedArrayEasy.java
+++ bytopic/array/java/MergeSortedArrayEasy.java
@@ -8,7 +8,7 @@
 // Output: [1,2,2,3,5,6]
 // Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 // The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
-// Soln: two pointer
+// Soln: append and sort
 // Time complexity ~ O((m+n)log(m+n)), due to sort() function
 // Space complexity ~ O(1)
 

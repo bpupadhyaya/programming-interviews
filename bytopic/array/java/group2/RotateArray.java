8387fe23d1ee897015c414be74325083be14bfc0
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-07 20:21:30 -0800

    Rotate array.

---
+++ bytopic/array/java/group2/RotateArray.java
@@ -0,0 +1,38 @@
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+// Example:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+import java.util.Arrays;
+class ArrayRotation {
+    public static void main(String...args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k,nums.length - 1);
+
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+// Logic:

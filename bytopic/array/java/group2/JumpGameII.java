7953efd100e42829a78d89c8981b3719995d44c4
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-08 17:31:46 -0800

    Jump game II. Work on TODO

---
+++ bytopic/array/java/group2/JumpGameII.java
@@ -0,0 +1,37 @@
+// You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+// Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are
+// at nums[i], you can jump to any nums[i + j] where:
+// 0 <= j <= nums[i] and
+// i + j < n
+// Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach
+// nums[n - 1].
+// Example:
+// Input: nums = [2,3,1,1,4]
+//Output: 2
+//Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1,
+// then 3 steps to the last index.
+// Tag: 10/150
+// TODO : change variable names to more readable
+
+class JumpGameII {
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println("Min number of jumps: " + jump(nums));
+    }
+
+    static int jump(int[] nums) {
+        int sc = 0;
+        int e = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
+            if (i == e) {
+                sc++;
+                e = max;
+            }
+        }
+        return sc;
+    }
+}
+
+// Logic:

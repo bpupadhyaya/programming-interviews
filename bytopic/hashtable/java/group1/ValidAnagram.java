d82a5b9cc66f0e8e3d4ce1dbd3d5e3b813b71242
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-12 17:13:25 -0800

    Find if two strings are anagram.

---
+++ bytopic/hashtable/java/group1/ValidAnagram.java
@@ -0,0 +1,29 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+// typically using all the original letters exactly once.
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+// Tag: 42/150
+
+import java.util.Arrays;
+class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println("Valid anagram? " + isAnagram(s, t));
+    }
+
+    static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+}

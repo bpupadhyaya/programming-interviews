d297fbdd145fc7dfdd6d6cad1a9e379b33b3bdfe
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-14 12:20:32 -0800

    Palindrome number detection, numerical method.

---
+++ bytopic/math/java/group1/PalindromeNumber.java
@@ -0,0 +1,29 @@
+// Given an integer, return true if the integer is a palindrome, and false if otherwise.
+// Sample 1:
+// Input 242
+// Output: true
+// Sample 2:
+// Input: -242
+// Output: false, explanation: from lef it reads -242, from right it reads 242- and hence not a palindrome.
+//
+// Tag: 131/150
+// Tag: 9/2927, R11/2936 (overall frequency ranking)
+class PalindromeNumber {
+    public static void main(String...args) {
+        int myNumber = 242;
+        System.out.println("Number: " + myNumber + " is palindrome (T/F): " + isPalindromeNumber(myNumber));
+    }
+
+    static boolean isPalindromeNumber(int x) {
+        if (x < 0 || x != 0 && x % 10 == 0)
+            return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return (x == rev || x == rev / 10);
+    }
+}

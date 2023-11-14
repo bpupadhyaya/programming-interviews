027fdad6e641664e4fdd6b9b573d584c7b6c8b5c
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-14 11:13:49 -0800

    Add binary.

---
+++ bytopic/bitmanipulation/java/group1/AddBinary.java
@@ -0,0 +1,32 @@
+// Given two binary strings a and b, return their sum as a binary string.
+// Sample 1:
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+//
+// Tag: 125/150
+// Tag: 67/2927, R356/2936 (overall frequency ranking)
+
+class AddBinary {
+    public static void main(String...args) {
+        String a = "1010";
+        String b = "1011";
+        System.out.println("Sum: " + addBinary(a,b));
+    }
+
+    static String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            result.append(sum % 2);
+        }
+        if (carry != 0) result.append(carry);
+
+        return result.reverse().toString();
+    }
+}

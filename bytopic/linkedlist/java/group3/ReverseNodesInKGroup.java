3b90ad64df314954db4508a19e6339866f305fdf
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-14 17:30:49 -0800

    Reverse Nodes in k-Group.

---
+++ bytopic/linkedlist/java/group3/ReverseNodesInKGroup.java
@@ -0,0 +1,67 @@
+// Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+// k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is
+// not a multiple of k then left-out nodes, in the end, should remain as it is.
+// You may not alter the values in the list's nodes, only nodes themselves may be changed.
+// Example 1:
+// Input: head = [1,2,3,4,5], k = 2
+// Output: [2,1,4,3,5]
+// Example 2:
+// Input: head = [1,2,3,4,5], k = 3
+// Output: [3,2,1,4,5]
+// Note: Visualize to understand
+//
+// Tag: 62/150
+// Tag: 25/2927, R150/2936 (overall frequency ranking)
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    public ListNode(int value) {
+        this.value = value;
+    }
+    public ListNode(int value, ListNode next) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedListReversalForRange {
+    public static void main(String...args) {
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(2);
+        list.next.next = new ListNode(3);
+        list.next.next.next = new ListNode(4);
+        list.next.next.next.next = new ListNode(5);
+        int k = 2;
+        ListNode result = reverseKGroup(list, k);
+        while (result.next != null) {
+            System.out.print(result.value + ",");
+            result = result.next;
+        }
+        System.out.print(result.value); // Last value
+        System.out.println();
+    }
+
+    static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while (curr != null && count != k) {
+            curr = curr.next;
+            count++;
+        }
+
+        if (count == k) {
+            curr = reverseKGroup(curr, k);
+            while (count-- > 0) {
+                ListNode tmp = head.next;
+                head.next = curr;
+                curr = head;
+                head = tmp;
+            }
+            head = curr;
+        }
+
+        return head;
+    }
+}

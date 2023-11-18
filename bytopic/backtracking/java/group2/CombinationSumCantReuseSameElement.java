673e04a9aa3d793560dedbf9ff8e9707e280f1a2
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-18 12:12:48 -0800

    Combination Sum. Can't reuse the same element.

--- bytopic/backtracking/java/group2/CombinationSumCantReuseSameElement.java
+++ bytopic/backtracking/java/group2/CombinationSumCantReuseSameElement.java
@@ -6,7 +6,7 @@
 // combinations for the given input.
 // Example:
 // Input: candidates = [2,3,6,7], target = 7
-// Output: [[2,2,3],[7]]
+// Output: [[7]]
 // Explanation:
 // 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
 // 7 is a candidate, and 7 = 7.

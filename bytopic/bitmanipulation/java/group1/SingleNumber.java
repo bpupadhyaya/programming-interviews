edf35133b7a232e593365a6127231dfe0a402e0f
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-19 13:39:33 -0800

    Single Number.

---
+++ bytopic/bitmanipulation/java/group1/SingleNumber.java
@@ -0,0 +1,24 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+// Example:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+//
+// Tag: 128/150
+// Tag: 136/2927, R138/2936 (overall frequency ranking)
+
+class SingleNumber {
+    public static void main(String...args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println("Single number: " + singleNumber(nums));
+    }
+
+    static int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+
+        return ans;
+    }
+}

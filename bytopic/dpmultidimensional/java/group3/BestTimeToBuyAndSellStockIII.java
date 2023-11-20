0c25ce5cff5f28f78ef23346b88777c4d3312577
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-19 20:08:23 -0800

    Best Time to Buy and Sell Stock III.

--- bytopic/dpmultidimensional/java/group3/BestTimeToBuyAndSellStockIII.java
+++ bytopic/dpmultidimensional/java/group3/BestTimeToBuyAndSellStockIII.java
@@ -17,6 +17,23 @@ class BestTimeToBuyAndSellStockIII {
     }
 
     static int maxProfit(int[] prices) {
-        // TODO
+        if (prices.length == 0)
+            return 0;
+        int k = 2;
+        int dp[][] = new int[k+1][prices.length];
+        for (int i = 1; i < dp.length; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < dp[0].length; j++) {
+                maxDiff = Math.max(maxDiff, dp[i-1][j-1] - prices[j-1]);
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + maxDiff);
+            }
+        }
+
+        return dp[k][prices.length - 1];
     }
 }
+
+// Time ~ O(k * n)
+// Space ~ O(k * n)
+// k = K transactions, 2 in this case
+// n = prices.length

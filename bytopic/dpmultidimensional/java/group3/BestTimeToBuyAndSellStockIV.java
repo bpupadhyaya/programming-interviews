cd95589db24f302bc97ce507166dde9d09cef767
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-19 20:16:55 -0800

    Best Time to Buy and Sell Stock IV.

--- bytopic/dpmultidimensional/java/group3/BestTimeToBuyAndSellStockIV.java
+++ bytopic/dpmultidimensional/java/group3/BestTimeToBuyAndSellStockIV.java
@@ -19,6 +19,30 @@ class BestTimeToBuyAndSellStockIV {
     }
 
     static int maxProfit(int k, int[] prices) {
-        // TODO
+        if (k >= prices.length / 2)
+            return maxProfit(prices);   // This is to avoid Memory Limit Exceeded... In other words,
+                                        // we can make infinite transactions. Using variant presented in Infinite
+                                        // number of transactions case.
+        int dp[][] = new int[k+1][prices.length];
+        for (int i = 1; i < dp.length; i++) {
+            int maxDiff = -prices[0];
+            for (int j = 1; j < dp[0].length; j++) {
+                maxDiff = Math.max(maxDiff, dp[i-1][j-1] - prices[j-1]);
+                dp[i][j] = Math.max(dp[i][j-1], prices[j] + maxDiff);
+            }
+        }
+
+        return dp[k][prices.length -1];
+    }
+
+    private static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i+1] > prices[i]) {
+                profit += prices[i+1] - prices[i];
+            }
+        }
+
+        return profit;
     }
 }

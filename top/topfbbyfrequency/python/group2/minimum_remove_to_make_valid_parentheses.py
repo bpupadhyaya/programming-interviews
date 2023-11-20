4e5e97498981a1aa19819be0d639151e928995f3
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-20 10:57:46 -0800

    Steps added.

--- top/topfbbyfrequency/python/group2/minimum_remove_to_make_valid_parentheses.py
+++ top/topfbbyfrequency/python/group2/minimum_remove_to_make_valid_parentheses.py
@@ -38,3 +38,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""
+Steps:
+1. Convert string to list, because String is an immutable data structure in Python and it's much easier and 
+memory-efficient to deal with a list for this task.
+2. Iterate through list
+3. Keep track of indices with open parentheses in the stack. In other words, when we come across open parenthesis we 
+add an index to the stack.
+4. When we come across close parenthesis we pop an element from the stack. If the stack is empty we replace current 
+list element with an empty string
+5. After iteration, we replace all indices we have in the stack with empty strings, because we don't have close 
+parentheses for them.
+6. Convert list to string and return
+"""

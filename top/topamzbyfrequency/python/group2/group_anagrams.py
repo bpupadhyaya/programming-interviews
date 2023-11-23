a5ab41d8b0daa377e0d8c3b0233ac0811fb80fb0
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-22 20:32:30 -0800

    Group Anagrams.

---
+++ top/topamzbyfrequency/python/group2/group_anagrams.py
@@ -0,0 +1,33 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+
+Example:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Tag: 49/2927 , R40/2935 , R4/50 (amz)
+"""
+from collections import defaultdict
+
+
+def group_anagrams(strs: list[str]) -> list[list[str]]:
+    anagram_map = defaultdict(list)
+
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        anagram_map[sorted_word].append(word)
+
+    return list(anagram_map.values())
+
+
+def main():
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    print(group_anagrams(strs))
+
+
+if __name__ == "__main__":
+    main()
+

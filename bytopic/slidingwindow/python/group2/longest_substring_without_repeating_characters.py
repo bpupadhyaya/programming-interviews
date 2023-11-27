e2f9d25285218b357c7f999b9b6d2f2faa52680d
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-26 21:03:34 -0800

    Ranking added.

--- bytopic/slidingwindow/python/group2/longest_substring_without_repeating_characters.py
+++ bytopic/slidingwindow/python/group2/longest_substring_without_repeating_characters.py
@@ -1,13 +1,17 @@
-# Given a string s, find the length of the longest substring without repeating characters.
-# Example 1: Input: s = "pwwkew"
-# Output: 3
-# Explanation: The answer is "wke", with the length of 3.
-# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-#  Example 2:
-# Input: s = "abcabcbb"
-# Output: 3
-# Explanation: The answer is "abc", with the length of 3.
-# Tag: 31/150
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+Example 1: Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+Example 2:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Tag: 31/150, 3/2927 R8/2935
+"""
+
 
 def length_of_longest_substring(s: str) -> int:
     n = len(s)
@@ -23,9 +27,11 @@ def length_of_longest_substring(s: str) -> int:
 
     return max_length
 
+
 def main():
     s = "abcabcbb"
     print("Length of longest substring: ", length_of_longest_substring(s))
+
 
 if __name__ == "__main__":
     main()

5cef59cfb3d9000876292c241bfe1ad508710b6a
bpupadhyaya <bpupadhyaya@gmail.com>
2023-11-27 17:50:50 -0800

    Spiral Matrix.

---
+++ bytopic/matrix/python/group2/spiral_matrix.py
@@ -0,0 +1,44 @@
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+Note: Visualize to understand
+
+Tag: 35/150
+Tag: 54/2927, R22/2936 (overall frequency ranking)
+"""
+
+
+def spiral_order(matrix: list[list[int]]) -> list[int]:
+    if not matrix:
+        return []
+    rows, cols = len(matrix), len(matrix[0])
+    top, bottom, left, right = 0, rows - 1, 0, cols - 1
+    result = []
+    while len(result) < rows * cols:
+        for i in range(left, right + 1):
+            result.append(matrix[top][i])
+        top += 1
+        for i in range(top, bottom + 1):
+            result.append(matrix[i][right])
+        right -= 1
+        if top <= bottom:
+            for i in range(right, left - 1, - 1):
+                result.append(matrix[bottom][i])
+            bottom -= 1
+        if left <= right:
+            for i in range(bottom, top - 1, - 1):
+                result.append(matrix[i][left])
+            left += 1
+    return result
+
+
+def main():
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(spiral_order(matrix))
+
+
+if __name__ == "__main__":
+    main()

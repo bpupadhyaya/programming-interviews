4d06d41e427eeb298c63cf5c8f8220336d274cc5
bpupadhyaya <bpupadhyaya@gmail.com>
2023-12-09 12:15:17 -0800

    Implementation note added for two pointer.

--- bytopic/array/python/group3/trapping_rain_water.py
+++ bytopic/array/python/group3/trapping_rain_water.py
@@ -61,10 +61,54 @@ def trap_using_dynamic_programming(height: list[int]) -> int:
     # Space complexity: O(n)
 
 
+def trap_using_two_pointer(height: list[int]) -> int:
+    if len(height) <= 2:
+        return 0
+    ans = 0
+    # Use two pointers i and j on indices 1 and n-1
+    i = 1
+    j = len(height) - 1
+    # Initialize left max to the leftmost bar and right max to the rightmost bar
+    left_max = height[0]
+    right_max = height[-1]
+
+    while i <= j:
+        # Check left_max and right_max for current i, j positions
+        if height[i] > left_max:
+            left_max = height[i]
+        if height[j] > right_max:
+            right_max = height[j]
+
+        # Fill water upto left_max level for index i and move i to the right
+        if left_max <= right_max:
+            ans += left_max - height[i]
+            i += 1
+
+        # Fill water upto right_max level for index j and move j to the left
+        else:
+            ans += right_max - height[j]
+            j -= 1
+
+    return ans
+
+
 def main():
     height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-    print(trap_using_dynamic_programming(height))
+    print(trap_using_two_pointer(height))
 
 
 if __name__ == "__main__":
     main()
+
+"""
+Implementation note for two pointer:
+Time complexity O(n)
+Space complexity O(1)
+
+Water units trapped in each index of the array is calculated and added individually.
+Water level at an index is determined by the lower of max_left and max_right for any bar,i.e. essentially water 
+trapped in bar i depends on min(max_left, right_left).
+Water trapped in index i = min(max_left, max_right) - height[i].
+Therefore if max_left < max_right, we fill the left index up to max_left, and advance the left pointer; and 
+vice versa. If max_left equals max_right, moving either pointer would work.
+"""

5f75f26abea6352d795da357791bc87f54cbd7a8
bpupadhyaya <bpupadhyaya@gmail.com>
2023-12-23 14:15:25 -0800

    Valid sudoku.

---
+++ top/topinterviewproblemsbyfrequency/python/group2/valid_sudoku_R66.py
@@ -0,0 +1,58 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the
+following rules:
+1. Each row must contain the digits 1-9 without repetition.
+2. Each column must contain the digits 1-9 without repetition.
+3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example:
+Input: board =
+ [["8","3",".",".","7",".",".",".","."]
+ ,["6",".",".","1","9","5",".",".","."]
+ ,[".","9","8",".",".",".",".","6","."]
+ ,["8",".",".",".","6",".",".",".","3"]
+ ,["4",".",".","8",".","3",".",".","1"]
+ ,["7",".",".",".","2",".",".",".","6"]
+ ,[".","6",".",".",".",".","2","8","."]
+ ,[".",".",".","4","1","9",".",".","5"]
+ ,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there
+are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Tag: R66/145
+Tag: 34/150
+Tag: 36/2927, R168/2936 (overall frequency ranking)
+"""
+
+
+def is_valid_sudoku(board: list[list[str]]) -> bool:
+    res = []
+    for i in range(9):
+        for j in range(9):
+            element = board[i][j]
+            if element != '.':
+                res += [(i, element), (element, j), (i // 3, j // 3, element)]
+    return len(res) == len(set(res))
+
+
+def main():
+    board = [
+        ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]
+    print(is_valid_sudoku(board))
+
+
+if __name__ == "__main__":
+    main()

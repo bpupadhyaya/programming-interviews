573580b1a2ac70e54c990d9213f868334239d138
bpupadhyaya <bpupadhyaya@gmail.com>
2023-12-30 14:08:43 -0800

    Set approach added.

--- top/topinterviewproblemsbyfrequency/python/group2/longest_substring_without_repeating_characters_R8.py
+++ top/topinterviewproblemsbyfrequency/python/group2/longest_substring_without_repeating_characters_R8.py
@@ -25,9 +25,27 @@ def length_of_longest_substring(s: str) -> int:
     return max_length
 
 
+def length_of_longest_substring_set(s: str) -> int:
+    n = len(s)
+    max_length = 0
+    char_set = set()
+    left = 0
+
+    for right in range(n):
+        if s[right] not in char_set:
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        else:
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left += 1
+            char_set.add(s[right])
+    return max_length
+
+
 def main():
     s = "pwwkew"
-    print(length_of_longest_substring(s))
+    print(length_of_longest_substring_set(s))
 
 
 if __name__ == "__main__":
@@ -42,6 +60,20 @@ If the character has occurred, we move the left pointer to the next position aft
 We update the index of the current character in charIndex.
 At each step, we update the maxLength by calculating the length of the current substring.
 We continue the iteration until reaching the end of the string.
+Finally, we return the maxLength as the length of the longest substring without repeating characters.
+
+
+Set approach:
+We use a set (charSet) to keep track of unique characters in the current substring.
+We maintain two pointers, left and right, to represent the boundaries of the current substring.
+The maxLength variable keeps track of the length of the longest substring encountered so far.
+We iterate through the string using the right pointer.
+If the current character is not in the set (charSet), it means we have a new unique character.
+We insert the character into the set and update the maxLength if necessary.
+If the character is already present in the set, it indicates a repeating character within the current substring.
+In this case, we move the left pointer forward, removing characters from the set until the repeating character is 
+no longer present.
+We insert the current character into the set and continue the iteration.
 Finally, we return the maxLength as the length of the longest substring without repeating characters.
 
 """

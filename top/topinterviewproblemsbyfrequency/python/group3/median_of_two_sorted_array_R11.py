5cfa7ed7627d27abad8eb6e4de25e28c1fb5cb9e
bpupadhyaya <bpupadhyaya@gmail.com>
2023-12-30 20:36:44 -0800

    Two pointer method added.

--- top/topinterviewproblemsbyfrequency/python/group3/median_of_two_sorted_array_R11.py
+++ top/topinterviewproblemsbyfrequency/python/group3/median_of_two_sorted_array_R11.py
@@ -73,10 +73,44 @@ def find_median_sorted_arrays(nums1: list[int], nums2: list[int]) -> float:
     return 0  # If the code reaches here, the input arrays were not sorted.
 
 
+def find_median_sorted_arrays_1(nums1: list[int], nums2: list[int]) -> float:
+    # Two pointer
+    n = len(nums1)
+    m = len(nums2)
+    i = 0
+    j = 0
+    m1 = 0
+    m2 = 0
+
+    # Find median.
+    for count in range(0, (n + m) // 2 + 1):
+        m2 = m1
+        if i < n and j < m:
+            if nums1[i] > nums2[j]:
+                m1 = nums2[j]
+                j += 1
+            else:
+                m1 = nums1[i]
+                i += 1
+        elif i < n:
+            m1 = nums1[i]
+            i += 1
+        else:
+            m1 = nums2[j]
+            j += 1
+
+    # Check if the sum of n and m is odd.
+    if (n + m) % 2 == 1:
+        return float(m1)
+    else:
+        ans = float(m1) + float(m2)
+        return ans / 2.0
+
+
 def main():
     nums1 = [1, 2]
     nums2 = [3, 4]
-    print(find_median_sorted_arrays(nums1, nums2))
+    print(find_median_sorted_arrays_1(nums1, nums2))
 
 
 if __name__ == "__main__":
@@ -97,6 +131,14 @@ Time Complexity:
 O(logm/logn)
 Space Complexity:
 O(1)
+
+Two-Pointer Method:
+Initialize two pointers, i and j, both initially set to 0.
+Move the pointer that corresponds to the smaller value forward at each step.
+Continue moving the pointers until you have processed half of the total number of elements.
+Calculate and return the median based on the values pointed to by i and j.
+Time Complexity: O(n + m), where ‘n’ & ‘m’ are the sizes of the two arrays.
+Space Complexity: O(1).
 """
 
 

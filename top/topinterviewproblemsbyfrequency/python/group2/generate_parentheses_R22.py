091b27198291c6d671e6c1dedd34e79dfbdb953f
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-01 11:41:35 -0800

    Backtracking implementation added.

--- top/topinterviewproblemsbyfrequency/python/group2/generate_parentheses_R22.py
+++ top/topinterviewproblemsbyfrequency/python/group2/generate_parentheses_R22.py
@@ -23,9 +23,35 @@ def generate_parentheses(n: int) -> list[str]:
     return res
 
 
+def generate_parentheses_1(n: int) -> list[str]:
+    ans=[]
+
+    def back_tracking(curr, open_brackets, close_brackets):
+        # Base case
+        if open_brackets == close_brackets == n:
+            ans.append("".join(curr))
+            return
+
+        # close_brackets is less than open_brackets
+        if close_brackets < open_brackets:
+            curr.append(")")
+            back_tracking(curr, open_brackets, close_brackets + 1)
+            curr.pop()
+
+        # open_brackets is less than the input
+        if open_brackets < n:
+            curr.append("(")
+            back_tracking(curr, open_brackets + 1, close_brackets)
+            curr.pop()
+
+    back_tracking(["("], 1, 0)
+
+    return ans
+
+
 def main():
     n = 3
-    print(generate_parentheses(n))
+    print(generate_parentheses_1(n))
 
 
 if __name__ == "__main__":
@@ -38,4 +64,17 @@ The idea is to add ')' only after valid '('
 We use two integer variables left & right to see how many '(' & ')' are in the current string
 If left < n then we can add '(' to the current string
 If right < left then we can add ')' to the current string
+
+generate_parentheses_1 explanation:
+Intuition
+We can just start thinking on this problem by drawing decision try and by observing the tree you may figure it out 
+that this is a Back-Tracking challenge!
+
+Approach
+Just we will start by adding an opening bracket. To have balanced parenthesis we need to have equal number of 
+opening and closing brackets.
+Two things to remember:
+
+We can add an opening bracket when the number of opening brackets is less than n.
+We can add closing bracket only when count of open brackets is more than closing brackets.
 """

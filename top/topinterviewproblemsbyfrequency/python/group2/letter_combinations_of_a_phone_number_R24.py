16c61cd889a58334b59aee14fd050f2f247a5e03
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-01 20:05:30 -0800

    Missing mapping, in problem, added.

--- top/topinterviewproblemsbyfrequency/python/group2/letter_combinations_of_a_phone_number_R24.py
+++ top/topinterviewproblemsbyfrequency/python/group2/letter_combinations_of_a_phone_number_R24.py
@@ -5,7 +5,7 @@ could represent. Return the answer in any order.
 A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to
 any letters.
 
-{ { ->00}, {2 -> abc}, {3 -> def}, {4 -> ghi}, {5 -> jkl}, {6 -> mno}, {7 -> pqrs}, {8 -> tuv}, {9 -> wxyz}
+{ {1 ->00}, {2 -> abc}, {3 -> def}, {4 -> ghi}, {5 -> jkl}, {6 -> mno}, {7 -> pqrs}, {8 -> tuv}, {9 -> wxyz}
 }
 
 Example:

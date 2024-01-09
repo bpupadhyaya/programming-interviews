eb3f69cbb940b130f7b61a3fba85e46acbcf203e
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-09 15:51:03 -0800

    Minimum Deletions to Make Character Frequencies Unique

--- top/topmsftbyfrequency/python/group2/minimum_deletions_to_make_character_frequencies_unique_M8.py
+++ top/topmsftbyfrequency/python/group2/minimum_deletions_to_make_character_frequencies_unique_M8.py
@@ -40,7 +40,7 @@ def min_deletions(s: str) -> int:
 def min_deletions1(s: str) -> int:
     freq = [0] * 26  # Create a list to store character frequencies
     for c in s:
-        freq[ord(c) - ord('a')] += 1 # Count the frequency of each character
+        freq[ord(c) - ord('a')] += 1  # Count the frequency of each character
 
     freq.sort()  # Sort frequencies in ascending order
     del_count = 0  # Initialize the deletion count

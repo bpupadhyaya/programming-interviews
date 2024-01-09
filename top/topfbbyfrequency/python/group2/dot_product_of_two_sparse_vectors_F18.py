ba8de85ac950d3c0be6c0cdf94587731d30916d7
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-08 22:26:51 -0800

    Re-formatting.

--- top/topfbbyfrequency/python/group2/dot_product_of_two_sparse_vectors_F18.py
+++ top/topfbbyfrequency/python/group2/dot_product_of_two_sparse_vectors_F18.py
@@ -19,12 +19,14 @@ v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
 
 Tag: fb R18/50, 1570/2927, R243/2936.
 """
+
+
 class SparseVector:
     def __init__(self, nums: list[int]):
         self.nums = {i: n for i, n in enumerate(nums) if n}
 
     # return the dotProduct of two sparse vectors
-    def dotProduct(self, vec: 'SparseVector') -> int:
+    def dot_product(self, vec: 'SparseVector') -> int:
         result = 0
         if len(self.nums) < len(vec.nums):
             for key in self.nums.keys():
@@ -42,7 +44,7 @@ def main():
     nums2 = [0, 3, 0, 4, 0]
     v1 = SparseVector(nums1)
     v2 = SparseVector(nums2)
-    ans = v1.dotProduct(v2)
+    ans = v1.dot_product(v2)
     print('Dot product: ', ans)
 
 

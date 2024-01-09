362f75afb9a1e9c9649b2fb6dde3148a0619583b
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-08 22:51:45 -0800

    LRU Cache

---
+++ top/topmsftbyfrequency/python/group2/lru_cache_M3.py
@@ -0,0 +1,102 @@
+"""
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair
+to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+Example:
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+
+Constraints:
+1 <= capacity <= 3000
+0 <= key <= 10^4
+0 <= value <= 10^5
+At most 2 * 10^5 calls will be made to get and put.
+
+Tag: R9/145, M3/50
+"""
+from collections import OrderedDict
+from typing import Dict
+
+
+class LRUCache:
+    __slots__ = ('data', 'capacity')
+
+    def __init__(self, capacity: int):
+        self.data: Dict[int, int] = OrderedDict()
+        self.capacity: int = capacity
+
+    def get(self, key: int) -> int:
+        return -1 if key not in self.data else self.data.setdefault(key, self.data.pop(key))
+
+    def put(self, key: int, value: int) -> None:
+        try:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        except KeyError:
+            self.data[key] = value
+            if len(self.data) > self.capacity:
+                self.data.popitem(last=False)
+
+
+class LRUCache1:
+    # Time complexity: O(1) for one operation.
+    # Space complexity: O(1) for one operation.
+    # For Python 3.7+ dictionary iteration order is guaranteed to be in order of insertion.
+    def __init__(self, capacity):
+        self.dict = {}
+        self.free_space = capacity
+
+    def get(self, key):
+        if key not in self.dict:
+            return -1
+        self.dict[key] = self.dict.pop(key)
+        return self.dict[key]
+
+    def put(self, key, value):
+        if key in self.dict:
+            self.dict.pop(key)
+        else:
+            if self.free_space:
+                self.free_space -= 1
+            else:
+                self.dict.pop(next(iter(self.dict)))
+        self.dict[key] = value
+
+
+def main():
+    lru_cache = LRUCache1(2)
+    lru_cache.put(1, 1)             # cache is {1=1}
+    lru_cache.put(2, 2)             # cache is {1=1, 2=2}
+    print(lru_cache.get(1))         # return 1
+    lru_cache.put(3, 3)             # LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    print(lru_cache.get(2))         # returns -1 (not found)
+    lru_cache.put(4, 4)             # LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    print(lru_cache.get(1))         # return -1 (not found)
+    print(lru_cache.get(3))         # return 3
+    print(lru_cache.get(4))         # return 4
+
+
+if __name__ == "__main__":
+    main()

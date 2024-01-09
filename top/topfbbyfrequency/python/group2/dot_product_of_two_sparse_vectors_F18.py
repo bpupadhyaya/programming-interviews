1ff504c66d931d79004e79bfaeb8cceeb2030fea
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-08 22:26:14 -0800

    Re-formatting.

--- top/topfbbyfrequency/python/group2/dot_product_of_two_sparse_vectors_F18.py
+++ top/topfbbyfrequency/python/group2/dot_product_of_two_sparse_vectors_F18.py
@@ -35,13 +35,16 @@ class SparseVector:
                 if key in self.nums:
                     result += self.nums[key] * vec.nums[key]
         return result
+
+
 def main():
-    nums1 = [1,0,0,2,3]
-    nums2 = [0,3,0,4,0]
+    nums1 = [1, 0, 0, 2, 3]
+    nums2 = [0, 3, 0, 4, 0]
     v1 = SparseVector(nums1)
     v2 = SparseVector(nums2)
     ans = v1.dotProduct(v2)
     print('Dot product: ', ans)
+
 
 if __name__ == "__main__":
     main()

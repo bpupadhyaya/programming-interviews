67e134c1c57980d32166b15e9307a432c5e5af58
bpupadhyaya <bpupadhyaya@gmail.com>
2024-01-15 15:20:05 -0800

    Formatting

--- top/topmsftbyfrequency/python/group2/spiral_matrix_M6.py
+++ top/topmsftbyfrequency/python/group2/spiral_matrix_M6.py
@@ -26,11 +26,11 @@ def spiral_order(matrix: list[list[int]]) -> list[int]:
             res.append(matrix[i][col_end])
         col_end -= 1
         if row_begin <= row_end:
-            for i in range(col_end, col_begin-1,-1):
+            for i in range(col_end, col_begin-1, -1):
                 res.append(matrix[row_end][i])
             row_end -= 1
         if col_begin <= col_end:
-            for i in range(row_end, row_begin-1,-1):
+            for i in range(row_end, row_begin-1, -1):
                 res.append(matrix[i][col_begin])
             col_begin += 1
     return res

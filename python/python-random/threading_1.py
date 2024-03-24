a9df50ce36c26ee6bb79f421a6613b166e23c0c1
bpupadhyaya <bpupadhyaya@gmail.com>
2024-03-24 10:38:14 -0700

    Python threading.

--- python/python-random/threading_1.py
+++ python/python-random/threading_1.py
@@ -1,9 +1,21 @@
 from threading import Thread
+from time import sleep
 
 
 def my_worker():
-    print('hi')
+    for i in range(0, 10):
+        print('.', end='', flush=True)
+        sleep(1)
 
 
+print('starting thread...')
 t1 = Thread(target=my_worker)
 t1.start()
+t1.join()
+print('\nDone')
+
+print(t1.getName())
+print(t1.ident)
+
+
+

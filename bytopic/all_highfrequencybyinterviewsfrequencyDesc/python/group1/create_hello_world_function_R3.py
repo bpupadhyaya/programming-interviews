e782f21b7df408f6e9b1097c512049f1791f407b
bpupadhyaya <bpupadhyaya@gmail.com>
2026-02-01 18:40:55 -0800

    Create Hello World Function. Python.

---
+++ bytopic/all_highfrequencybyinterviewsfrequencyDesc/python/group1/create_hello_world_function_R3.py
@@ -0,0 +1,20 @@
+"""
+Write a function createHelloWorld. It should return a new function that always
+returns "Hello World".
+
+Input: args = [{},null,42]
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f({}, null, 42); // "Hello World"
+
+Any arguments could be passed to the function, but it should always
+return "Hello World".
+
+Constraints:
+0 <= args.length <= 10
+
+Tag: 2667/2927, R3/2936 (overall frequency ranking)
+Cat: g1
+"""
+
